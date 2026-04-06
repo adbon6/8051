fee633e3ff85a531e6521dc1c5a3a6e268254d49
A_dbon <adbon6@gmail.com>
2026-04-06 21:09:17 +0530

    review1 (#3)
    * review1
    
    * Remove redundant lines and clean up code
    
    * Update header file from reg51.h to reg52.h

---
+++ review1.c
@@ -0,0 +1,45 @@
+#include <reg52.h>
+
+sbit LED1 = P1^0;
+sbit SWITCH1 = P1^5;
+sbit LED2 = P1^1;
+sbit SWITCH2 = P1^6;
+sbit SWITCH3 = P1^7;
+
+void delay(unsigned int ms) {
+    unsigned int i, j;
+    for (i = 0; i < ms; i++)
+        for (j = 0; j < 120; j++);
+}
+
+void main(void) {
+    while (1) {
+        if (SWITCH1 == 0) {
+            LED1 = 0; 
+        } else {
+            LED1 = 1; 
+        }
+				if (SWITCH2 == 0) {
+            LED2 = 0; 
+        } else {
+            LED2 = 1;
+        }
+				if (SWITCH3 == 0){
+						LED1 = 0;
+						delay(1000);
+						LED2 = 0;
+						delay(1000);
+						LED1 = 1;
+						delay(1000);
+				} else {
+						LED1 = 1;
+						LED2 = 1;
+				}
+		
+    }
+}
+		
+
+
+
+
